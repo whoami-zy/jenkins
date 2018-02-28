7427b12b5607442d85aeda3879f1e9f8045a69d1
Your Name <you@example.com>
2018-02-27 21:44:59 -0800

    first commit

---
+++ src/main/java/com/jenkins/JenkinsTest/App.java
@@ -0,0 +1,13 @@
+package com.jenkins.JenkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Jenkins Test" );
+    }
+}
