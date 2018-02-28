65fa07a0cbb9ce6370aad3ca68429a75aab2f375
Your Name <you@example.com>
2018-02-27 22:11:28 -0800

    2commit

--- src/main/java/com/jenkins/JenkinsTest/App.java
+++ src/main/java/com/jenkins/JenkinsTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Jenkins Test" );
+        System.out.println( "Hello World! Jenkins Test 第二次" );
     }
 }
